3fe14d9dd86588410167c5504363fbf13be033aa
SamuraiCego <samuraimik3@gmail.com>
2025-05-03 20:29:26 +0000

    Nivel Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,54 +1,62 @@
 #include <stdio.h>
+// Criando um Void para cada peça
+void torreR(int movimentoT){
+    if (movimentoT > 0){
+        printf("Direita\n");
+        torreR(movimentoT - 1);
+    }
+}
+void bispoR(int movimentoB){
+    if (movimentoB > 0){
+        printf("Cima, Direita\n");
+        bispoR(movimentoB - 1);
+    }
+}
+void rainhaR(int movimentoR){
+    if (movimentoR > 0){
+        printf("Esquerda\n");
+        rainhaR(movimentoR - 1);
+    }
+}
 
 int main(){
 
-// Declarando previamente a variavel das peças
-int torre, bispo = 1, rainha = 1, cavalo;
+printf("Movimento da Torre:\n"); // Printf para identificar a peça que está se mexendo
+torreR(5); // iniciando torreR com o valor de 5
+printf("\n"); // Printf para pular a linha no console
 
+printf("Movimento do Bispo com Recursividade:\n"); // Printf para identificar a peça que está se mexendo
+bispoR(5); // iniciando bispoR com o valor de 5
+printf("\n"); // Printf para pular a linha no console
 
-printf("Movimento da Torre:\n"); // printf para identificar qual peça está se movendo no momento
-
-for (torre = 1; torre <= 5; torre++){
-    printf("Direita\n"); // Demonstrando o movimento da torre
-    
-} // utilizando for para imprimir "direita" 5 vezes, parando o código assim que a variavel torre atinge o valor de 6
-
-printf("\n"); // Pulando uma linha para separar cada movimento de peça
-
-printf("Movimento do Bispo:\n"); // printf para identificar qual peça está se movendo no momento
-
-while (bispo <= 5){ 
-    printf("Cima, Direita\n"); // Demonstrando o movimento do bispo
-    bispo++;
-    
-} // utilizando while para imprimir "Cima, Direita" 5 vezes, parando o código assim que a variavel bispo atinge o valor de 6
-
-printf("\n"); // Pulando uma linha para separar cada movimento de peça
-
-printf("Movimento da Rainha:\n"); // printf para identificar qual peça está se movendo no momento
-
-do{
-    printf("Esquerda\n"); // Demonstrando o movimento da rainha
-    rainha++;
-    
-}while(rainha <=8); // utilizando do while para imprimir "Esquerda" 8 vezes, parando o código assim que a variavel torre atinge o valor de 9
-
-printf("\n"); // Pulando uma linha para separar cada movimento de peça
-
-printf("Movimento Cavalo:\n"); // printf para identificar qual peça está se movendo no momento
-
-for(cavalo=1; cavalo<=1; cavalo++){ // utilizando for para executar o movimento de baixo duas vezes dentro do loop interno antes de imprimir o movimento de esquerda
-       do{
-        printf("Baixo\n"); 
-        cavalo++;
-       }while(cavalo<=2); // utilizando do while para imprimir 'baixo' e depois adicionar 1 a variavel cavalo, repetindo até cavalo ser 3
-       printf("Esquerda\n"); // imprimindo 'esquerda' após o do while imprimir baixo duas vezes
-       
-
+printf("Movimento do Bispo com Loops aninhados:\n"); // Printf para identificar a peça que está se mexendo
+/* Utilizando o loop externo para imprimir "Cima," e acrescentar a variavel bispoCima até que ela se torne 6, e utilizando o loop interno para imprimir "Direita" enquanto 
+a variavel bispoLado tem o valor igual a variavel bispoCima e acrescentando a ela, fazendo  com que elas não tenham mais o valor igual e então parando o loop interno*/
+for(int bispoCima = 1; bispoCima <= 5; bispoCima++){ 
+    printf("Cima, ");
+    for (int bispoLado = bispoCima; bispoLado == bispoCima; bispoLado++){
+        printf("Direita\n");
+    }
 }
+printf("\n"); // Printf para pular a linha no console
 
+printf("Movimento da Rainha\n"); // Printf para identificar a peça que está se mexendo
+rainhaR(8); // iniciando rainhaR com o valor de 8
+printf("\n"); // Printf para pular a linha no console
 
-
+printf("Movimento do Cavalo\n");
+/*utilizando o for com as variaveis cavaloCima e cavaloLado, enquanto cavaloLado for igual a 1 o loop externo imprimirá "Cima" e acrescentará em cavaloCima, o for interno 
+apenas executará quando cavaloCima tiver o valor de 2 e cavaloLado tiver o valor de 1 e quando for executado imprimirá "Direita" e acrescentará em cavaloLado fazendo com que 
+ambos os loops parem de executar
+*/
+for(int cavaloCima=1, cavaloLado=1; cavaloLado == 1; cavaloCima++){ 
+    printf("Cima\n");
+    
+    for(cavaloLado; cavaloCima == 2 && cavaloLado == 1; cavaloLado++){
+        printf("Direita\n");
+    }
+    
+}
 
 return 0;
 
